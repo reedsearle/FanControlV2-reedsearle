8c74523d595feb60c68060b4bafd74e96c88a3e4
Reed Searle <csearle@cnm.edu>
2024-02-15 12:49:05 -0700

    Refactored constants, started changing MQTT functions

--- src/FanControlV2.cpp
+++ src/FanControlV2.cpp
@@ -31,10 +31,12 @@ Adafruit_MQTT_SPARK mqtt(&TheClient,AIO_SERVER,AIO_SERVERPORT,AIO_USERNAME,AIO_K
 
 /****************************** Feeds ***************************************/ 
 // Setup Feeds to publish or subscribe 
-Adafruit_MQTT_Publish   mqttObj1 = Adafruit_MQTT_Publish  (&mqtt, AIO_USERNAME "/feeds/nmmep.dialValue");
-Adafruit_MQTT_Publish   mqttObj2 = Adafruit_MQTT_Publish  (&mqtt, AIO_USERNAME "/feeds/nmmep.opampValue");
-Adafruit_MQTT_Publish   mqttObj3 = Adafruit_MQTT_Publish  (&mqtt, AIO_USERNAME "/feeds/nmmep.relayValue");
-Adafruit_MQTT_Publish   mqttObj4 = Adafruit_MQTT_Publish  (&mqtt, AIO_USERNAME "/feeds/nmmep.switchValue");
+Adafruit_MQTT_Publish   mqttObj1 = Adafruit_MQTT_Publish  (&mqtt, AIO_USERNAME "/feeds/nmmep.signcompany-relayValue");
+Adafruit_MQTT_Publish   mqttObj2 = Adafruit_MQTT_Publish  (&mqtt, AIO_USERNAME "/feeds/nmmep.signcompany-relayState");
+Adafruit_MQTT_Publish   mqttObj3 = Adafruit_MQTT_Publish  (&mqtt, AIO_USERNAME "/feeds/nmmep.signcompany-switchState");
+Adafruit_MQTT_Publish   mqttObj4 = Adafruit_MQTT_Publish  (&mqtt, AIO_USERNAME "/feeds/nmmep.signcompany-VOCReturn");
+Adafruit_MQTT_Publish   mqttObj5 = Adafruit_MQTT_Publish  (&mqtt, AIO_USERNAME "/feeds/nmmep.signcompany-dataValid");
+Adafruit_MQTT_Publish   mqttObj6 = Adafruit_MQTT_Publish  (&mqtt, AIO_USERNAME "/feeds/nmmep.signcompany-eveningRun");
 
 
 Adafruit_MQTT_Subscribe theTemperatureObject = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/nmmep.signcompany-temperature");
@@ -46,20 +48,27 @@ Adafruit_MQTT_Subscribe theResetObject       = Adafruit_MQTT_Subscribe(&mqtt, AI
 
 /************Declare Variables*************/
 
-const int relayPin = A0;  // Input of the relay sampler
-const int relayStatePin = A1;  // State of the relay; Manual, Auto, Floating
-const int switchStatePin = A2;  // State of the relay; Manual, Auto, Floating
+const int RELAY_PIN        = A0; // Input of the relay sampler
+const int RELAY_STATE_PIN  = A1; // State of the relay; Manual, Auto, Floating
+const int SWITCH_STATE_PIN = A2; // State of the relay; Manual, Auto, Floating
 
-const int WDT_ST_PIN     = D0;  //  Watchdog timer strobe output on Pin A1
-const int WDT_RSTN_PIN   = D1;  // Reset(low) from the DS1232. Used for instrumentation
-const int tpl5010Wake    = D2; // Wake pin from tpl5010
-const int tpl5010Done    = D3; // Done pin to tpl5010
-const int fanCtlOutPin   = D4;  //  Fan control output pin to OpAmp
-const int WDT_PBRSTN_PIN = D9;  // Reset(low) to the  DS1232. Used to control relay through BJT
+const int WDT_ST_PIN       = D0; // Watchdog timer strobe output on Pin A1
+const int WDT_RSTN_PIN     = D1; // Reset(low) from the DS1232. Used for instrumentation
+const int TPL5010_WAKE_PIN = D2; // Wake pin from tpl5010
+const int TPL5010_DONE_PIN = D3; // Done pin to tpl5010
+const int FAN_CTL_OUT_PIN  = D4; // Fan control output pin to OpAmp
+const int WDT_PBRSTN_PIN   = D9; // Reset(low) to the  DS1232. Used to control relay through BJT
 
-const int WDT_Half_Time  = 250; //  Half time pulse for WDT.  Used to change the state ot the WDT Strobe output
-bool      WDT_ST_State;         //  State of the watchdog timer strobe output
+const int WDT_Half_Time    = 250;//  Half time pulse for WDT.  Used to change the state ot the WDT Strobe output
+bool      WDT_ST_State;          //  State of the watchdog timer strobe output
 
+const int VOC_HI_LIMIT      = 350;  // VOC limit above which the fan will turn on - from Clint Wolf's Code
+const int HUMIDITY_LOW_LIMIT = 20;  //  Humidity limit to determine fan speed - from Clint Wolf's Code
+
+const int  FAN_OFF    = 0;   // 0.0 on manual fan dial
+const int  FAN_IDLE   = 65;  // 2.6 on manual fan dial
+const int  FAN_NORMAL = 90;  // 3.5 on manual fan dial
+const int  FAN_HIGH   = 200; // 8.0 on manual fan dial
 
 bool firstRead;  //  indicator of the first time the subscription has been read since the device turned on
 
@@ -68,35 +77,29 @@ int   humidity    = -1; // humidity variable set to Invalid Data
 int   VOC         = -1; // VOC variable set to Invalid Data
 int   CO2         = -1; // CO2 variable set to Invalid Data
 
-const int VOC_HiLimit      = 350;  // VOC limit above which the fan will turn on - from Clint Wolf's Code
-const int humidityLowLimit = 20;  //  Humidity limit to determine fan speed - from Clint Wolf's Code
-
 bool dataValid;   //  Flag for MQTT subscription data receiving on all four subscriptions
 bool eveningRun;  //  Flag for nightly high speed fan
-
-const int  fanOff    = 0;   // 0.0 on manual fan dial
-const int  fanIdle   = 65;  // 2.6 on manual fan dial
-const int  fanNormal = 90;  // 3.5 on manual fan dial
-const int  fanHigh   = 200; // 8.0 on manual fan dial
 
 //  Time & timing Variables
 String    dateTime, timeOnly, timeOnlyOld, hourOnly;   
 u_int64_t last, lastTime;  // These timing variables work with System.millis()
 
 //  Sample Variables
-  int dialSample;   //  Value of the manual dial output to the relay
-  int opampSample;  //  Value of the opamp output to the relay
   int relaySample;  //  Value of the output of the relay
-
-  int switchSample;  //  Current selected position of the manual/automatic switch;
+  int relayState;   //  Currrent state of the relay; manual, auto, or floating(BAD)
+  int switchState;  //  Current selected position of the manual/automatic switch;
+  int wdtResetnIn;  //  Current reset state of the Watch Dog Timer, DS1232 ACTIVE LOW
+  bool resetMQTT;   // Forced reset from dashboard through MQTT broker
 
 
 
 
 //  INSTANTIATIONS  //
 void watchDogISR();
-void MQTT_connect();
+void MQTT_connect(int* valid);
 bool MQTT_ping();
+void MQTT_publish(int relaySample, int relayState, int switchState, int VOC, bool dataValid, bool eveningRun);
+void MQTT_subscribe(int* temp, int* hum, int* volit, int* carbon, int* resetin);
 
 
 Timer watchDogTimer(WDT_Half_Time, watchDogISR);  //  Instantiate a Timer interrupt with period WDT_Half_Time and interrupt service routinte watchDogISR
@@ -110,12 +113,13 @@ void setup() {
   Serial.begin(9600);
   waitFor(Serial.isConnected, 15000); //wait for Serial Monitor to startup
 
-  pinMode(WDT_ST_PIN,    OUTPUT);
-  pinMode(WDT_PBRSTN_PIN, OUTPUT);
-  pinMode(fanCtlOutPin,  OUTPUT);
+  pinMode(WDT_ST_PIN,      OUTPUT);
+  pinMode(WDT_PBRSTN_PIN,  OUTPUT);
+  pinMode(WDT_RSTN_PIN,    INPUT);
+  pinMode(FAN_CTL_OUT_PIN, OUTPUT);
 
-  pinMode(tpl5010Done, OUTPUT);
-  pinMode(tpl5010Wake, INPUT);
+  pinMode(TPL5010_DONE_PIN, OUTPUT);
+  pinMode(TPL5010_WAKE_PIN, INPUT);
 
 
   digitalWrite(WDT_PBRSTN_PIN, HIGH);  //  Force Watchdog into reset
@@ -164,18 +168,26 @@ void loop() {
 
   /*  MQTT Function Block*/
 // Validate connected to MQTT Broker
-  MQTT_connect();
+  MQTT_connect(&dataValid); // Check connection.  Trip dataValid flag if connection lost
   MQTT_ping();
 
   // publish to cloud every 30 seconds
+  /* DATA TO PUBLISH:
+      relaySample
+      relayState
+      switchState
+      VOC 
+      dataValid
+      eveningRun
+  */
   if((System.millis()-lastTime > 30000)) {
-    MQTT_publish(float *value)
+    MQTT_publish(relaySample, relayState, switchState, VOC, dataValid, eveningRun);
     lastTime = millis();
   }
 
   // this is our 'wait for incoming subscription packets' busy subloop
   // Check for change in the four variables from the MQTT broker
-  MQTT_subscribe(*value);
+  MQTT_subscribe(&temperature, &humidity, &VOC, &CO2, &resetMQTT);
 
 
 
@@ -214,27 +226,27 @@ void loop() {
   // Evening fan run to remove latent VOCs
   // Supercedes all other requirements
   if (hourOnly == "20" && !eveningRun) {
-    analogWrite(fanCtlOutPin, fanHigh);  
+    analogWrite(FAN_CTL_OUT_PIN, FAN_HIGH);  
     Serial.printf("Evening run started at %s \n", timeOnly.c_str());
     eveningRun = TRUE;
   } else if (hourOnly != "20" && eveningRun) {
-      analogWrite(fanCtlOutPin, fanOff);  
+      analogWrite(FAN_CTL_OUT_PIN, FAN_OFF);  
     Serial.printf("Evening run ended at %s \n", timeOnly.c_str());
       eveningRun = FALSE;
   }
 
       // Fan is not running and VOC limit exceeded
       // - Inactive during evening run of fan
-      if (VOC > VOC_HiLimit && dataValid && !eveningRun) {
-        if (humidity > humidityLowLimit) {
-          analogWrite(fanCtlOutPin, fanNormal);  // Humidity is high so use higher fan speed
+      if (VOC > VOC_HI_LIMIT && dataValid && !eveningRun) {
+        if (humidity > HUMIDITY_LOW_LIMIT) {
+          analogWrite(FAN_CTL_OUT_PIN, FAN_NORMAL);  // Humidity is high so use higher fan speed
           Serial.printf("Fan Normal, VOC=%i, Humidity=%i \n", VOC, humidity);
         }  else {
-          analogWrite(fanCtlOutPin, fanIdle);  // Humidity is low so use lower fan speed
+          analogWrite(FAN_CTL_OUT_PIN, FAN_IDLE);  // Humidity is low so use lower fan speed
           Serial.printf("Fan Idle, VOC=%i, Humidity=%i \n", VOC, humidity);
         }
-      } else if (VOC <= VOC_HiLimit && !eveningRun) {
-        analogWrite(fanCtlOutPin, fanOff);  // VOCs have dropped below threshold, turn fan off
+      } else if (VOC <= VOC_HI_LIMIT && !eveningRun) {
+        analogWrite(FAN_CTL_OUT_PIN, FAN_OFF);  // VOCs have dropped below threshold, turn fan off
         Serial.printf("Fan off \n");
       }
    
@@ -246,9 +258,8 @@ void loop() {
 //  The input that most closely matches the output is determined to be the driver.
 /////////////////////////////////////////////////////////////////////////////
 
-  dialSample  = analogRead(dialPin);
-  opampSample = analogRead(opampPin);
-  relaySample = analogRead(relayPin);
+  relaySample = analogRead(RELAY_PIN);
+  relayState  = digitalRead(RELAY_STATE_PIN);
 
 /////////////////////////////////////////////////////////////////////////////
 //  The user has the option of running the fan in either manual mode or
@@ -256,7 +267,9 @@ void loop() {
 //  position of the switch can be read by the Argon
 //////////////////////////////////////////////////////////////////////////////
 
-  switchSample = digitalRead(switchPin);
+  switchState = digitalRead(SWITCH_STATE_PIN);
+
+  wdtResetnIn = digitalRead(WDT_RSTN_PIN); 
 
 
 } // loop
@@ -272,7 +285,7 @@ void loop() {
 //          MQTT_CONNECT
 //////////////////////////////////////////////////////////////
 // Function to connect and reconnect as necessary to the MQTT server.
-void MQTT_connect() {
+void MQTT_connect(int* valid) {
   int8_t ret;
 
   // Stop if already connected.
@@ -286,6 +299,7 @@ void MQTT_connect() {
       //  Serial.printf("%s\n",(char *)mqtt.connectErrorString(ret));
       //  Serial.printf("Retrying MQTT connection in 5 seconds..\n");
        mqtt.disconnect();
+       *valid = false;
        delay(5000);  // wait 5 seconds
   }
   // Serial.printf("MQTT Connected!\n");
@@ -313,13 +327,15 @@ bool MQTT_ping() {
 ///////////////////////////////////////////////////////////////
 //          MQTT_PUBLISH
 //////////////////////////////////////////////////////////////
-void MQTT_publish(float *value) {
+void MQTT_publish(int relaySample, int relayState, int switchState, int VOC, bool dataValid, bool eveningRun) {
     if(mqtt.Update()) {
-      mqttObj1.publish(dialSample);
-      mqttObj2.publish(opampSample);
-      mqttObj3.publish(relaySample);
-      mqttObj4.publish(switchSample);
-      Serial.printf("Publishing %0.2f \n",*value); 
+      mqttObj1.publish(relaySample);
+      mqttObj2.publish(relayState);
+      mqttObj3.publish(switchState);
+      mqttObj4.publish(VOC);
+      mqttObj3.publish(dataValid);
+      mqttObj4.publish(eveningRun);
+      // Serial.printf("Publishing %0.2f \n",*value); 
   }
 }
 
@@ -327,34 +343,33 @@ void MQTT_publish(float *value) {
 ///////////////////////////////////////////////////////////////
 //          MQTT_SUBSCRIBE
 //////////////////////////////////////////////////////////////
-void MQTT_subscribe(float *value){
-  float tempVar;
+void MQTT_subscribe(int* temp, int* hum, int* volit, int* carbon, int* resetin){
   // this is our 'wait for incoming subscription packets' busy subloop 
   Adafruit_MQTT_Subscribe *subscription;
   while ((subscription = mqtt.readSubscription(10))) {
     // Serial.printf("Inside subscribe\n");
     if (subscription == &theTemperatureObject) {
-      temperature = atof((char *)theTemperatureObject.lastread);
+      *temp = atof((char *)theTemperatureObject.lastread);
       // Serial.printf("Received %0.1f from Adafruit.io feed theTemperatureObject \n",temperature);
     }
 
     if (subscription == &theHumidityObject) {
-      humidity = atof((char *)theHumidityObject.lastread);
+      *hum = atof((char *)theHumidityObject.lastread);
       // Serial.printf("Received %i from Adafruit.io feed theHumidityObject \n",humidity);
     }
 
     if (subscription == &theTVOCObject) {
-      VOC = atof((char *)theTVOCObject.lastread);
+      *volit = atof((char *)theTVOCObject.lastread);
       // Serial.printf("Received %i from Adafruit.io feed theTVOCObject \n",VOC);
     }
 
     if (subscription == &theCO2Object) {
-      CO2 = atof((char *)theCO2Object.lastread);
+      *carbon = atof((char *)theCO2Object.lastread);
       // Serial.printf("Received %i from Adafruit.io feed theeCO2Object \n",CO2);
     }
 
     if (subscription == &theResetObject) {
-      CO2 = atof((char *)theResetObject.lastread);
+      *resetin = atof((char *)theResetObject.lastread);
       // Serial.printf("Received %i from Adafruit.io feed theResetObject \n",CO2);
     }
   }
