6de0f91e0ae0b95bd55c0a865d3365a92f7542bd
reedsearle <reed_searle@hotmail.com>
2024-02-23 16:46:14 -0700

    moved fan control code to function - NOT YET IMPLEMENTED

--- src/FanControlV2.cpp
+++ src/FanControlV2.cpp
@@ -57,18 +57,20 @@ const int WDT_RSTN_PIN     = D1; // Reset(low) from the DS1232. Used for instrum
 const int TPL5010_WAKE_PIN = D2; // Wake pin from tpl5010
 const int TPL5010_DONE_PIN = D3; // Done pin to tpl5010
 const int FAN_CTL_OUT_PIN  = D4; // Fan control output pin to OpAmp
+const int D7_LED_PIN       = D7; // Onboard LED
 const int WDT_PBRSTN_PIN   = D9; // Reset(low) to the  DS1232. Used to control relay through BJT
 
 const int WDT_Half_Time    = 250;//  Half time pulse for WDT.  Used to change the state ot the WDT Strobe output
 bool      WDT_ST_State;          //  State of the watchdog timer strobe output
 
-const int VOC_HI_LIMIT      = 350;  // VOC limit above which the fan will turn on - from Clint Wolf's Code
-const int HUMIDITY_LOW_LIMIT = 20;  //  Humidity limit to determine fan speed - from Clint Wolf's Code
-
 const int  FAN_OFF    = 0;   // 0.0 on manual fan dial
 const int  FAN_IDLE   = 65;  // 2.6 on manual fan dial
 const int  FAN_NORMAL = 90;  // 3.5 on manual fan dial
 const int  FAN_HIGH   = 200; // 8.0 on manual fan dial
+long startFanTest;
+const long FAN_TEST_DELAY = 5000;
+int fanTest[] = {FAN_OFF, FAN_IDLE, FAN_NORMAL,FAN_HIGH};
+int fanCount;
 
 bool firstRead;  //  indicator of the first time the subscription has been read since the device turned on
 
@@ -81,25 +83,23 @@ bool dataValid;   //  Flag for MQTT subscription data receiving on all four subs
 bool eveningRun;  //  Flag for nightly high speed fan
 
 //  Time & timing Variables
-String    dateTime, timeOnly, timeOnlyOld, hourOnly;   
 u_int64_t last, lastTime;  // These timing variables work with System.millis()
 
 //  Sample Variables
-  int relaySample;  //  Value of the output of the relay
-  int relayState;   //  Currrent state of the relay; manual, auto, or floating(BAD)
-  int switchState;  //  Current selected position of the manual/automatic switch;
-  int wdtResetnIn;  //  Current reset state of the Watch Dog Timer, DS1232 ACTIVE LOW
-  bool resetMQTT;   // Forced reset from dashboard through MQTT broker
-
-
-
+int relaySample;  //  Value of the output of the relay
+int relayState;   //  Currrent state of the relay; manual, auto, or floating(BAD)
+int switchState;  //  Current selected position of the manual/automatic switch;
+int wdtResetnIn;  //  Current reset state of the Watch Dog Timer, DS1232 ACTIVE LOW
+bool resetMQTT;   // Forced reset from dashboard through MQTT broker
 
 //  INSTANTIATIONS  //
 void watchDogISR();
 void MQTT_connect(bool* valid);
 bool MQTT_ping();
-void MQTT_publish(int relaySample, int relayState, int switchState, int VOC, bool dataValid, bool eveningRun);
+void MQTT_publish(int relaySample, int relayState, bool switchState, int VOC, bool dataValid, bool eveningRun);
 void MQTT_subscribe(float* temp, int* hum, int* volit, int* carbon, bool* resetin);
+void fanControl(float* temperature, int* humidity, int* VOC, int* CO2, 
+                bool* dataValid, int* FAN_CTL_OUT_PIN, bool* eveningRun);
 
 
 Timer watchDogTimer(WDT_Half_Time, watchDogISR);  //  Instantiate a Timer interrupt with period WDT_Half_Time and interrupt service routinte watchDogISR
@@ -120,6 +120,12 @@ void setup() {
 
   pinMode(TPL5010_DONE_PIN, OUTPUT);
   pinMode(TPL5010_WAKE_PIN, INPUT);
+
+  pinMode(D7_LED_PIN, OUTPUT);
+
+  pinMode(RELAY_PIN, INPUT);
+  pinMode(RELAY_STATE_PIN, INPUT);
+  pinMode(SWITCH_STATE_PIN, INPUT);
 
 
   digitalWrite(WDT_PBRSTN_PIN, HIGH);  //  Force Watchdog into reset
@@ -145,11 +151,6 @@ void setup() {
   dataValid  = FALSE;  //  Initialize dataValid flag to FALSE
   eveningRun = FALSE;  //  Initialize eveningRun flag to FALSE
 
-  //  Setup TIME
-  Time.zone(-6);                                         //  Set time zone to MDT -6 from UTC
-  Particle.syncTime();
-  dateTime    = Time.timeStr();                          //  get current value of date and time
-  timeOnlyOld = dateTime.substring(11,16);               //  Extract value of time from dateTime
 
   watchDogTimer.start();
 
@@ -160,11 +161,20 @@ void setup() {
 
 // loop() runs over and over again, as quickly as it can execute.
 void loop() {
+  // This is temporary
 
-  /*  Get current time*/
-  dateTime = Time.timeStr();                          //  get current value of date and time
-  timeOnly = dateTime.substring(11,16);               //  Extract value of time from dateTime
-  hourOnly = dateTime.substring(11,13);               //  Extract value of time from dateTime
+  digitalWrite(D7_LED_PIN, WDT_ST_State);
+  if(millis() - startFanTest > FAN_TEST_DELAY ){
+    analogWrite(FAN_CTL_OUT_PIN, fanTest[fanCount%4]);
+    Serial.printf("Fan speed %i\n", fanTest[fanCount%4]);
+    fanCount++;
+    startFanTest = millis();
+  }
+
+  // Read state of input pins
+  switchState = digitalRead(SWITCH_STATE_PIN);
+  relayState = analogRead(RELAY_STATE_PIN);
+  relaySample = analogRead(RELAY_PIN);
 
   /*  MQTT Function Block*/
 // Validate connected to MQTT Broker
@@ -193,63 +203,7 @@ void loop() {
 
   
   
-  //  Automatic Fan Control Output
-
-  ///////////////////////////////////////////////////////////////////////////
-  //  Fan control logic needs valid data to work with.  During startup, the
-  //  environmental data can be invalid for several minutes while the MQTT
-  //  broker connections are established.  All local environmental variables
-  //  are initialized to -1 to show they are invalid.  Once the MQTT broker
-  //  is online, the data will be valid and fan control may begin.
-  ////////////////////////////////////////////////////////////////////
-
-  //  Check for invalid data as indicated by a value of -1
-  if (temperature == -1 || humidity == -1 || VOC == -1 || CO2 == -1) {
-    dataValid = FALSE;
-    watchDogTimer.stop();
-  }  else if (!dataValid) {  // Data just turned valid
-    dataValid = TRUE;
-    digitalWrite(WDT_PBRSTN_PIN, LOW);  // Force watchdog to run.
-    watchDogTimer.start();              // Start watchdog keep-alive signal
-    // Serial.printf("Data Valid \n");
-
-  }
-
-  //////////////////////////////////////////////////////////////////////////
-  //  Fan control has two distinct phases: Evening phase where the fan is
-  //  run for an hour at night to remove all latent VOCs.  The fan is then turned
-  //  off to the remainder of the evening to allow the humidity to recover.
-  //  Normal operation phase checks for VOC peaks and runs the fan to remove 
-  //  VOCs with the fan speed dependant on the relative humidity.
-  ///////////////////////////////////////////////////////////////////////////
-
-  // Evening fan run to remove latent VOCs
-  // Supercedes all other requirements
-  if (hourOnly == "20" && !eveningRun) {
-    analogWrite(FAN_CTL_OUT_PIN, FAN_HIGH);  
-    Serial.printf("Evening run started at %s \n", timeOnly.c_str());
-    eveningRun = TRUE;
-  } else if (hourOnly != "20" && eveningRun) {
-      analogWrite(FAN_CTL_OUT_PIN, FAN_OFF);  
-    Serial.printf("Evening run ended at %s \n", timeOnly.c_str());
-      eveningRun = FALSE;
-  }
-
-      // Fan is not running and VOC limit exceeded
-      // - Inactive during evening run of fan
-      if (VOC > VOC_HI_LIMIT && dataValid && !eveningRun) {
-        if (humidity > HUMIDITY_LOW_LIMIT) {
-          analogWrite(FAN_CTL_OUT_PIN, FAN_NORMAL);  // Humidity is high so use higher fan speed
-          Serial.printf("Fan Normal, VOC=%i, Humidity=%i \n", VOC, humidity);
-        }  else {
-          analogWrite(FAN_CTL_OUT_PIN, FAN_IDLE);  // Humidity is low so use lower fan speed
-          Serial.printf("Fan Idle, VOC=%i, Humidity=%i \n", VOC, humidity);
-        }
-      } else if (VOC <= VOC_HI_LIMIT && !eveningRun) {
-        analogWrite(FAN_CTL_OUT_PIN, FAN_OFF);  // VOCs have dropped below threshold, turn fan off
-        Serial.printf("Fan off \n");
-      }
-   
+    
 
 ////////////////////////////////////////////////////////////////////////////
 //  Fan output measurement samples three locations to determine which device,
@@ -327,7 +281,7 @@ bool MQTT_ping() {
 ///////////////////////////////////////////////////////////////
 //          MQTT_PUBLISH
 //////////////////////////////////////////////////////////////
-void MQTT_publish(int relaySample, int relayState, int switchState, int VOC, bool dataValid, bool eveningRun) {
+void MQTT_publish(int relaySample, int relayState, bool switchState, int VOC, bool dataValid, bool eveningRun) {
     if(mqtt.Update()) {
       mqttObj1.publish(relaySample);
       mqttObj2.publish(relayState);
@@ -376,11 +330,90 @@ void MQTT_subscribe(float* temp, int* hum, int* volit, int* carbon, bool* reseti
 
 }
 
+
+///////////////////////////////////////////////////////////////
+//          FAN_CONTROL
+//////////////////////////////////////////////////////////////
+  //  Fan control logic needs valid data to work with.  During startup, the
+  //  environmental data can be invalid for several minutes while the MQTT
+  //  broker connections are established.  All local environmental variables
+  //  are initialized to -1 to show they are invalid.  Once the MQTT broker
+  //  is online, the data will be valid and fan control may begin.
+  ////////////////////////////////////////////////////////////////////
+void fanControl(float* temperature, int* humidity, int* VOC, int* CO2, 
+                bool* dataValid, int* FAN_CTL_OUT_PIN, bool* eveningRun) {
+
+  String    dateTime, timeOnly, timeOnlyOld, hourOnly;   
+  const int  FAN_OFF    = 0;   // 0.0 on manual fan dial
+  const int  FAN_IDLE   = 65;  // 2.6 on manual fan dial
+  const int  FAN_NORMAL = 90;  // 3.5 on manual fan dial
+  const int  FAN_HIGH   = 200; // 8.0 on manual fan dial
+
+  const int VOC_HI_LIMIT      = 350;  // VOC limit above which the fan will turn on - from Clint Wolf's Code
+  const int HUMIDITY_LOW_LIMIT = 20;  //  Humidity limit to determine fan speed - from Clint Wolf's Code
+
+  //  Setup TIME
+  Time.zone(-6);                                         //  Set time zone to MDT -6 from UTC
+  Particle.syncTime();
+  dateTime    = Time.timeStr();                          //  get current value of date and time
+  timeOnlyOld = dateTime.substring(11,16);               //  Extract value of time from dateTime
+
+
+  //  Check for invalid data as indicated by a value of -1
+  if (*temperature == -1 || *humidity == -1 || *VOC == -1 || *CO2 == -1) {
+    *dataValid = FALSE;
+    watchDogTimer.stop();
+  }  else if (!dataValid) {  // Data just turned valid
+    *dataValid = TRUE;
+    // digitalWrite(WDT_PBRSTN_PIN, LOW);  // Force watchdog to stop.
+    watchDogTimer.start();              // Start watchdog keep-alive signal
+    // Serial.printf("Data Valid \n");
+  }
+
+  //////////////////////////////////////////////////////////////////////////
+  //  Fan control has two distinct phases: Evening phase where the fan is
+  //  run for an hour at night to remove all latent VOCs.  The fan is then turned
+  //  off to the remainder of the evening to allow the humidity to recover.
+  //  Normal operation phase checks for VOC peaks and runs the fan to remove 
+  //  VOCs with the fan speed dependant on the relative humidity.
+  ///////////////////////////////////////////////////////////////////////////
+
+  // Evening fan run to remove latent VOCs
+  // Supercedes all other requirements
+  if (hourOnly == "20" && !eveningRun) {
+    analogWrite(*FAN_CTL_OUT_PIN, FAN_HIGH);  
+    Serial.printf("Evening run started at %s \n", timeOnly.c_str());
+    *eveningRun = TRUE;
+  } else if (hourOnly != "20" && eveningRun) {
+    analogWrite(*FAN_CTL_OUT_PIN, FAN_OFF);  
+    Serial.printf("Evening run ended at %s \n", timeOnly.c_str());
+    *eveningRun = FALSE;
+  }
+
+  // Fan is not running and VOC limit exceeded
+  // - Inactive during evening run of fan
+  if (*VOC > VOC_HI_LIMIT && *dataValid && !*eveningRun) {
+    if (*humidity > HUMIDITY_LOW_LIMIT) {
+      analogWrite(*FAN_CTL_OUT_PIN, FAN_NORMAL);  // Humidity is high so use higher fan speed
+      Serial.printf("Fan Normal, VOC=%i, Humidity=%i \n", *VOC, *humidity);
+    } else {
+      analogWrite(*FAN_CTL_OUT_PIN, FAN_IDLE);  // Humidity is low so use lower fan speed
+      Serial.printf("Fan Idle, VOC=%i, Humidity=%i \n", *VOC, *humidity);
+    }
+  } else if (*VOC <= VOC_HI_LIMIT && !*eveningRun) {
+    analogWrite(*FAN_CTL_OUT_PIN, FAN_OFF);  // VOCs have dropped below threshold, turn fan off
+    Serial.printf("Fan off \n");
+  }
+}
+
+
+
 /////////////////////////////////////////////
 //  Watchdog ISR function
 //////////////////////////////////////////////
 void watchDogISR() {
   WDT_ST_State = !WDT_ST_State;
   digitalWrite(WDT_ST_PIN, WDT_ST_State);
+  digitalWrite(TPL5010_DONE_PIN, WDT_ST_State);
   return;
 }
